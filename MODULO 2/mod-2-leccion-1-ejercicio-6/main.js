b54fdeca0a9b94c9351d65f18cb9c2315d2710a8
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-04-26 12:38:26 +0200

    add lesson 1 ex 6

---
+++ MODULO 2/mod-2-leccion-1-ejercicio-6/main.js
@@ -0,0 +1,3 @@
+const content = document.querySelector('.content');
+
+content.innerHTML= '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, explicabo iure perferendis molestiae illo, necessitatibus, distinctio qui voluptate eveniet tempore eius sit earum quod minima sapiente fugit vitae est ea?</p>';
