bdba75990fc20ed45501a14584a214b2fea7b48e
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-04-26 15:04:30 +0200

    add lesson 2 ex 3

--- MODULO 2/mod-2-leccion-2-ejercicio-3/main.js
+++ MODULO 2/mod-2-leccion-2-ejercicio-3/main.js
@@ -6,6 +6,6 @@ const cantKiwis=2;
 const cantPeras=3;
 const cantUvas= 0.5;
 
-const totalCompra = ((2*5)+(3*2)+(0.5*4));
+const totalCompra = ((cantKiwis*precioKiwis)+(cantPeras*precioPeras)+(cantUvas*precioUvas));
 
 console.log('totalCompra: ' + totalCompra + '€');
