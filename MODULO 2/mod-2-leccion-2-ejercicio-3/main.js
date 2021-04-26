9e225ec52bf6f4978235f3efd8adf2cf4e8e0337
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-04-26 14:57:59 +0200

    add lesson 2 ex 3

---
+++ MODULO 2/mod-2-leccion-2-ejercicio-3/main.js
@@ -0,0 +1,11 @@
+const precioKiwis = 5;
+const precioPeras = 2;
+const precioUvas = 4;
+
+const cantKiwis=2;
+const cantPeras=3;
+const cantUvas= 0.5;
+
+const totalCompra = ((2*5)+(3*2)+(0.5*4));
+
+console.log('totalCompra: ' + totalCompra + '€');
