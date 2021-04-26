8914e5c29b78523ee2328f37e8a47ce60d9406d8
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-04-26 15:08:43 +0200

    add lesson 2 ex 4

---
+++ MODULO 2/mod-2-leccion-2-ejercicio-5/main.js
@@ -0,0 +1,7 @@
+const edad = 29;
+const diasAño = 365;
+const horasDía = 24;
+
+const horasVividas = edad*diasAño*horasDía;
+
+console.log('Si tengo ' + edad + ' años,he vivido: ' + horasVividas + ' horas.');
