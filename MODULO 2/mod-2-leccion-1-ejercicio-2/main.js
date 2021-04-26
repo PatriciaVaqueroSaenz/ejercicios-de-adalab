cd5fc1d964f6c8ddd4de4c7435d50fb4843be901
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-04-26 11:15:01 +0200

    add lesson 1 ex 2

---
+++ MODULO 2/mod-2-leccion-1-ejercicio-2/main.js
@@ -0,0 +1,7 @@
+// const mainAddress = 'Calle Pedro Muñoz Seca, 2';
+// mainAddress= 'Calle de Espoz y Mina, 7'
+
+//Uncaught TypeError: Assignment to constant variable.
+
+let mainAddress = 'Calle Pedro Muñoz Seca, 2';
+mainAddress= 'Calle de Espoz y Mina, 7'
