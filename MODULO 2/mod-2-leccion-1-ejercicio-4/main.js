65c4e467d7fd56f0e12bc74ec867a823dd380179
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-04-26 12:11:17 +0200

    add lesson 1 ex 4

---
+++ MODULO 2/mod-2-leccion-1-ejercicio-4/main.js
@@ -0,0 +1,4 @@
+const titleElement = document.querySelector('.title');
+const adalaberSelected = document.querySelector('.item2')
+
+titleElement.innerHTML = titleElement.innerHTML + adalaberSelected.innerHTML;
