27f514365218cd8e4fec5e00bbd5f6a8e9160967
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-01 11:01:10 +0200

    add lesson 2 ex 9

---
+++ MODULO 2/mod-2-leccion-2-ejercicio-9/main.js
@@ -0,0 +1,3 @@
+const partner = "Lorena Pérez Martínez";
+const text = document.querySelector('.text');
+text.innerHTML= 'Mi compañera es: '+ partner + ' y está compuesto por ' +partner.length+ ' caracteres.' 
