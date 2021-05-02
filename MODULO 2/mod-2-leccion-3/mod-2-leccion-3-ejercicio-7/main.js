0d6a40fc626ba061fe48bcc44ee2df698f67bec7
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-02 13:01:50 +0200

    add lesson 3 ex 7

---
+++ MODULO 2/mod-2-leccion-3/mod-2-leccion-3-ejercicio-7/main.js
@@ -0,0 +1,15 @@
+let ingredient = "Carne";
+
+switch (ingredient) {
+  case "Pollo":
+    console.log("Tu ingrediente es pollo. Receta: Filete con patatas");
+    break;
+  case "Merluza":
+    console.log("Tu ingrediente es merluza. Receta: Merlucita en salsa verde");
+    break;
+  case "Espinacas":
+    console.log("Tu ingrediente es espinacas. Receta: Espinacas rehogadas");
+    break;
+  default:
+    console.log("Nada en la nevera");
+}
