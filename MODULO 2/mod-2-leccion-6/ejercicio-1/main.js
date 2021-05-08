98c347ff77b75f61af155118be624eae0dc14c3d
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-08 12:31:56 +0200

    add lesson 6 ex1

---
+++ MODULO 2/mod-2-leccion-6/ejercicio-1/main.js
@@ -0,0 +1,15 @@
+const adalaber1 = {};
+adalaber1.name = 'Susana';
+adalaber1.age = 34;
+adalaber1.job = 'periodista';
+
+
+console.log(`Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} años y soy ${adalaber1.job}.`);
+
+
+const adalaber2 = {};
+adalaber2.name = 'Rocío';
+adalaber2.age = 25;
+adalaber2.job = 'actriz';
+
+console.log(`Mi nombre es ${adalaber2.name}, tengo ${adalaber2.age} años y soy ${adalaber2.job}.`);
