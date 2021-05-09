d072ec91686a95c86450ddb0a93e62743ffb544e
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-09 18:48:13 +0200

    add lesson 7 ex4

---
+++ MODULO 2/mod-2-leccion-7/ejercicio-4/main.js
@@ -0,0 +1,12 @@
+let hunterMoon=2017;
+
+
+for (let i=0; i<15; i++){
+    hunterMoon+=3;
+    console.log('La siguiente luna del cazador será el 5 de octubre de: '+hunterMoon);
+}
+
+//Primero definir el año en el que ha sucedido la ultima luna
+//Segundo crear el bucle para las siguientes quince lunas
+//Tercero dentro del bucle tenemos que incrementar el año en tres.
+// Cuarto: imprimir en pantalla los siguientes 15 años en los que se podrá ver la luna
