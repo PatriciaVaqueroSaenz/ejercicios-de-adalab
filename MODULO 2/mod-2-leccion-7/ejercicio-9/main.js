1578ae7e2bd41397bdc694cdab756f37fb7cffd8
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-09 22:01:45 +0200

    add lesson 7 ex9

---
+++ MODULO 2/mod-2-leccion-7/ejercicio-9/main.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('h1')
+// NodeList(11) [h1.reset-3c756112--pageTitle-33dc39a3, h1#contenidos.blockHeading-442b4f12, h1#introduccion.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#en-que-casos-se-utilizan.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#array.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#trabajando-con-arrays.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#bucles.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#bucle-for-of.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#queryselectorall.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#bonus-for-in.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e, h1#recursos-externos-adicionales.blockHeading-442b4f12--blockHeading1WithMargin-b69d755e]
+
+
+document.querySelectorAll('h1')[0]
+// <h1 class=​"reset-3c756112--pageTitle-33dc39a3">​…​</h1>​
+
+document.querySelectorAll('h1')[0].className
+// "reset-3c756112--pageTitle-33dc39a3"
+
+document.querySelectorAll('h1')[0].innerText
+// "2.7 Arrays y bucles"
+
+document.querySelectorAll('asdf')
+// NodeList []length: 0__proto__: NodeList
