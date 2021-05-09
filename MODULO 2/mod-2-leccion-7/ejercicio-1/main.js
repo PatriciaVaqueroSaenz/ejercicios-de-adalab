f3379baafcae557104fb827aac39ced9cc9e5eac
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-09 18:10:46 +0200

    add lesson 7 ex1

---
+++ MODULO 2/mod-2-leccion-7/ejercicio-1/main.js
@@ -0,0 +1,11 @@
+function workWithMovies(){
+
+    const movies = ['Into the wild', 'Captain fantastic', 'Amelie'];
+    movies[3]= 'El rey león';
+    console.log(movies);
+    movies[3]='Harry Potter';
+    console.log(movies);
+
+}
+
+workWithMovies();
