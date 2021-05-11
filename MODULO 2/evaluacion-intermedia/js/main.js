a6be59119815e96bedfcf9b2fd3d372640afdd00
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-11 12:55:38 +0200

    add auto-indent

--- MODULO 2/evaluacion-intermedia/js/main.js
+++ MODULO 2/evaluacion-intermedia/js/main.js
@@ -1,55 +1,49 @@
-'use strict';
+"use strict";
 
-const button = document.querySelector('.js-button');
-const faceSelect = document.querySelector('.js-select');
-const faceElement  = document.querySelector('.js-face');
-const mainElement = document.querySelector('.js-page')
+const button = document.querySelector(".js-button");
+const faceSelect = document.querySelector(".js-select");
+const faceElement = document.querySelector(".js-face");
+const mainElement = document.querySelector(".js-page");
 
-function refreshFace(){
-    const selectedFace=faceSelect.value;
-    if(selectedFace === 'happy'){
-        faceElement.innerHTML = ':)';
-    }else{
-        faceElement.innerHTML = ':(';
-    }
+function refreshFace() {
+  const selectedFace = faceSelect.value;
+  if (selectedFace === "happy") {
+    faceElement.innerHTML = ":)";
+  } else {
+    faceElement.innerHTML = ":(";
+  }
 }
 
-function generateRandomNumber(){
+function generateRandomNumber() {
+  const number = Math.round(Math.random() * 100);
 
-    const number = Math.round(Math.random() * 100);
-
-    return number;
+  return number;
 }
 
-function removeBackgroundClass(){
-
-    mainElement.classList.remove('correctYellow','chileanOrange');
+function removeBackgroundClass() {
+  mainElement.classList.remove("correctYellow", "chileanOrange");
 }
 
-function setNewBackground(number){
-
-    if(number%2===0){
-        mainElement.classList.add('correctYellow');
-    }else{
-        mainElement.classList.add('chileanOrange');
-    }
+function setNewBackground(number) {
+  if (number % 2 === 0) {
+    mainElement.classList.add("correctYellow");
+  } else {
+    mainElement.classList.add("chileanOrange");
+  }
 }
 
-function refreshBackground(){
+function refreshBackground() {
+  const number = generateRandomNumber();
 
-    const number = generateRandomNumber();
-    
-    removeBackgroundClass();
-    
-    setNewBackground(number);
+  removeBackgroundClass();
+
+  setNewBackground(number);
 }
 
-function handleClickButton(){
-    
-    refreshFace();
-    
-    refreshBackground();
+function handleClickButton() {
+  refreshFace();
+
+  refreshBackground();
 }
 
-
-button.addEventListener('click',handleClickButton);
+button.addEventListener("click", handleClickButton);
