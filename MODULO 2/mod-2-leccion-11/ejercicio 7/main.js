4a9b376d50878ba9317930e674c024612a6163c6
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-17 22:54:02 +0200

    add lesson 11 ex 7

---
+++ MODULO 2/mod-2-leccion-11/ejercicio 7/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const inputName = document.querySelector('.js-inputName');
+const inputLastName = document.querySelector('.js-inputLastName');
+const text = document.querySelector('.js-text');
+
+
+function fillText(){
+
+    let inputNameValue = inputName.value; 
+    let inputLastNameValue = inputLastName.value;
+    text.innerHTML= inputNameValue + ' ' + inputLastNameValue ;
+    
+    localStorage.setItem('name',inputNameValue);
+    localStorage.setItem('lastName',inputLastNameValue);
+
+}
+
+
+const savedName = localStorage.getItem('name');
+const savedLastName = localStorage.getItem('lastName');
+
+if(savedName!=='' && savedLastName!=='' && savedName!==null && savedLastName!==null){
+    inputName.value = savedName;
+    inputLastName.value = savedLastName;
+    text.innerHTML = savedName + ' ' + savedLastName;
+}else{
+    inputName.value = '';
+    inputLastName.value = '';
+    text.innerHTML = '';
+}
+
+inputName.addEventListener('keyup',fillText);
+inputLastName.addEventListener('keyup',fillText);
