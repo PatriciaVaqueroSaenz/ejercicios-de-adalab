cc0d467c472af2856c8274b88d7e1267a87b6383
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-17 23:03:23 +0200

    add lesson 11 ex 8

---
+++ MODULO 2/mod-2-leccion-11/ejercicio 8/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// En la web aparece una caja de texto donde escribimos el nombre de un personaje (o parte del nombre) y un botón, al hacer click, nuestra web muestra debajo un listado con los personajes que coinciden con la búsqueda indicando su nombre y género.
+
+// coger info del botón y del input
+const button = document.querySelector('.js-button');
+
+function findCharacter() {
+  const inputName = document.querySelector('.js-input').value;
+
+  fetch(`https://swapi.dev/api/people/?search=${inputName}`)
+    .then((searchResponse) => searchResponse.json())
+    .then((searchData) => {
+        //guardo los personajes obtenidos accediendo a results
+      const characters = searchData.results;
+        //guardaré esos personajes en una lista
+      const characterList = document.querySelector('.js-characters');
+      let content = '';
+
+      for (let i = 0; i < characters.length; i++) {
+        const name = characters[i].name;
+        const gender = characters[i].gender;
+
+        content += `<li>Character's name is ${name} and its gender is ${gender}</li>`;
+      }
+      //guardo los personajes coincidentes en la lista
+      characterList.innerHTML = content;
+    });
+}
+
+// function saveSearch() {
+//     localStorage.setItem('userSearch', inputName.value);
+//   }
+
+button.addEventListener('click', findCharacter);
