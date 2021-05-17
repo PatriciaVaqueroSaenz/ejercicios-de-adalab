f15bace93389121b8351500a74fa50bd14e77bd1
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-17 22:35:08 +0200

    add lesson 11 ex 5

---
+++ MODULO 2/mod-2-leccion-11/ejercicio-5/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const form = document.querySelector('.js-form')
+const content = document.querySelector('.content');
+
+function theme(event){
+    const checked = event.target.value;
+    
+    if(checked === '1'){
+        content.classList.remove('js-dark');
+        content.classList.add('js-light');
+    }else{
+        content.classList.remove('js-light');
+        content.classList.add('js-dark');
+    }
+
+    localStorage.setItem('theme', checked);
+}
+
+let savedTheme = localStorage.getItem('theme');
+if (savedTheme === '1'){
+    content.classList.remove('js-dark');
+    content.classList.add('js-light');
+}else{
+    content.classList.remove('js-light');
+    content.classList.add('js-dark');
+}
+
+form.addEventListener('click', theme);
+
+
