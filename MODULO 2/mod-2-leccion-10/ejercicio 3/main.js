4dd6d124e479fc8bf8b81d97d2249ff767c8337d
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-17 06:36:35 +0200

    add lesson 10 ex3

--- MODULO 2/mod-2-leccion-10/ejercicio 3/main.js
+++ MODULO 2/mod-2-leccion-10/ejercicio 3/main.js
@@ -1,2 +1,22 @@
 'use strict';
 
+
+function getUser() {
+  const username = document.querySelector('.js-user').value;
+
+  fetch(`https://api.github.com/users/${username}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const img = document.querySelector('img');
+      img.src = data.avatar_url;
+
+      const repositories = document.querySelector('.js-info');
+      repositories.innerHTML = `Este usuario ha publicado ${data.public_repos} repositorios.`;
+
+      const name = document.querySelector('.js-name');
+      name.innerHTML = data.name;
+    });
+}
+
+const btn = document.querySelector('.js-btn-search');
+btn.addEventListener('click', getUser);
