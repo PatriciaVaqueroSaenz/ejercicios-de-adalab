c510232dcc25a1e52a182c06dc2da61faa70dd89
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-18 10:27:44 +0200

    add linter

--- MODULO 2/mod-2-leccion-11/ejercicio 5/main.js
+++ MODULO 2/mod-2-leccion-11/ejercicio 5/main.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const form = document.querySelector('.js-form')
+const form = document.querySelector('.js-form');
 const content = document.querySelector('.content');
 
 function theme(event){
-    const checked = event.target.value;
-    
-    if(checked === '1'){
-        content.classList.remove('js-dark');
-        content.classList.add('js-light');
-    }else{
-        content.classList.remove('js-light');
-        content.classList.add('js-dark');
-    }
+  const checked = event.target.value;
 
-    localStorage.setItem('theme', checked);
+  if(checked === '1'){
+    content.classList.remove('js-dark');
+    content.classList.add('js-light');
+  }else{
+    content.classList.remove('js-light');
+    content.classList.add('js-dark');
+  }
+
+  localStorage.setItem('theme', checked);
 }
 
 let savedTheme = localStorage.getItem('theme');
 if (savedTheme === '1'){
-    content.classList.remove('js-dark');
-    content.classList.add('js-light');
+  content.classList.remove('js-dark');
+  content.classList.add('js-light');
 }else{
-    content.classList.remove('js-light');
-    content.classList.add('js-dark');
+  content.classList.remove('js-light');
+  content.classList.add('js-dark');
 }
 
 form.addEventListener('click', theme);
