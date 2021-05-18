dd97fa73b560ce4aae18d1f1d199e8b2538dd429
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-18 11:26:32 +0200

    add lesson 11 ex 7bis

--- MODULO 2/mod-2-leccion-11/ejercicio 7/main.js
+++ MODULO 2/mod-2-leccion-11/ejercicio 7/main.js
@@ -7,12 +7,12 @@ const text = document.querySelector('.js-text');
 
 function fillText(){
 
-    let inputNameValue = inputName.value; 
-    let inputLastNameValue = inputLastName.value;
-    text.innerHTML= inputNameValue + ' ' + inputLastNameValue ;
-    
-    localStorage.setItem('name',inputNameValue);
-    localStorage.setItem('lastName',inputLastNameValue);
+  let inputNameValue = inputName.value;
+  let inputLastNameValue = inputLastName.value;
+  text.innerHTML= inputNameValue + ' ' + inputLastNameValue ;
+
+  localStorage.setItem('name',inputNameValue);
+  localStorage.setItem('lastName',inputLastNameValue);
 
 }
 
@@ -21,13 +21,13 @@ const savedName = localStorage.getItem('name');
 const savedLastName = localStorage.getItem('lastName');
 
 if(savedName!=='' && savedLastName!=='' && savedName!==null && savedLastName!==null){
-    inputName.value = savedName;
-    inputLastName.value = savedLastName;
-    text.innerHTML = savedName + ' ' + savedLastName;
+  inputName.value = savedName;
+  inputLastName.value = savedLastName;
+  text.innerHTML = savedName + ' ' + savedLastName;
 }else{
-    inputName.value = '';
-    inputLastName.value = '';
-    text.innerHTML = '';
+  inputName.value = '';
+  inputLastName.value = '';
+  text.innerHTML = '';
 }
 
 inputName.addEventListener('keyup',fillText);
