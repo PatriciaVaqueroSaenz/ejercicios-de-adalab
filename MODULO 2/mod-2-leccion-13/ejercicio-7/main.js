7a19cb9229fa29d50db9a96306d5878872d43f35
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-18 23:54:36 +0200

    lesson 13 ex 7

---
+++ MODULO 2/mod-2-leccion-13/ejercicio-7/main.js
@@ -0,0 +1,13 @@
+const times = [56, 9, 45, 28, 35];
+
+var result = times.reduce(function (total, item, indice, array) {
+  total = total + item;
+
+  if(indice === array.length - 1) {
+      return total / array.length;
+  }
+    
+  return total;
+});
+
+console.log(result);
