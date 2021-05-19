5af949d2198881021a74c19500f7d3f3674a0a92
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-19 11:55:02 +0200

    lesson 12 ex 12

---
+++ MODULO 2/mod-2-leccion-13/ejercicio-12/main.js
@@ -0,0 +1,10 @@
+const users = [
+    { name: 'María', isPremium: false, pin: 2389 },
+    { name: 'Lucía', isPremium: true, pin: 2384 },
+    { name: 'Susana', isPremium: true, pin: 2837 },
+    { name: 'Rocío', isPremium: false, pin: 5232 },
+    { name: 'Inmaculada', isPremium: false, pin: 8998 }
+];
+
+users.sort((a, b) => a - b);
+console.log(users);
