b6323ec1e10afa07cb07170ce85fc69bef3444ee
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-19 07:48:41 +0200

    lesson 13 ex 7

--- MODULO 2/mod-2-leccion-13/ejercicio-7/main.js
+++ MODULO 2/mod-2-leccion-13/ejercicio-7/main.js
@@ -1,7 +1,6 @@
 const times = [56, 9, 45, 28, 35];
 
-var result = times.reduce(function (total, item, indice, array) {
-  total = total + item;
+var result = times.reduce(function (total, item, indice, array) { total = total + item;
 
   if(indice === array.length - 1) {
       return total / array.length;
