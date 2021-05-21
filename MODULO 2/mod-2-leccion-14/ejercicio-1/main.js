0ade331956d310ca33897cba583d984be73d1a9d
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-21 07:15:29 +0200

    add lesson14 ex 1

---
+++ MODULO 2/mod-2-leccion-14/ejercicio-1/main.js
@@ -0,0 +1,9 @@
+let counter = 0;
+
+const incrementAndShowCounter = () => {
+  counter+=2;
+  const time = document.querySelector('.time');
+  time.innerHTML = counter;
+};
+
+setInterval(incrementAndShowCounter, 2000);
