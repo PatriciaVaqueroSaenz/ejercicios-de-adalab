c652eccf59acafb2a25640203d3ae483cd1892ae
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-31 10:08:43 +0200

    ex2

--- MODULO 3/mod-3-leccion-1/ejercicio-2/src/App.js
+++ MODULO 3/mod-3-leccion-1/ejercicio-2/src/App.js
@@ -2,37 +2,47 @@
 import React from "react";
 import foto from "./foto.jpg";
 import "./App.css";
-// Vamos a crear un nuevo proyecto de React llamado mediacard. Vamos a modificar el método render del componente App para que tenga un diseño similar al de la imagen. Podéis usar una imagen a vuestra elección en lugar de la que aparece en el diseño, y Font-Awesome para el icono del corazón. De esta forma, aprenderemos a cómo trabajar con cosas que ya conocemos (HTML y CSS) en una aplicación de React.
+
 // get our fontawesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
+const Header = (
+  <header className="App-header">
+    <img src={foto} className="App-foto" alt="foto" />
+    <div className="App-header-title">
+      <h1 className="App-header-maintitle">Patricia Vaquero</h1>
+      <h3 className="App-header-subtitle">Lunes 26 de junio de 2017</h3>
+    </div>
+  </header>
+);
+const Main = (
+  <main className="App-main">
+    <p className="App-main-paragraph">
+      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their
+      default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </p>
+  </main>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={foto} className="App-foto" alt="foto" />
-        <div className="App-header-title">
-          <h1 className="App-header-maintitle">Patricia Vaquero </h1>
-          <h3 className="App-header-subtitle">Lunes 26 de junio de 2017</h3>
-        </div>
-      </header>
-      <main className="App-main">
-        <p className="App-main-paragraph">
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-          default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-      </main>
-      <footer className="App-footer">
-        <p className="App-footer-paragraph">Leer más...</p>
+const Footer = (
+  <footer className="App-footer">
+     <p className="App-footer-paragraph">Leer más...</p>
         <div className="App-footer-container">
           <p className="App-footer-paragraph">37</p>
           <FontAwesomeIcon icon={faHeart} className="App-icon"/>
         </div>
-      </footer>
+  </footer>
+);
+const appRoot = (
+  <div className="App">
+      {Header}
+      {Main}
+      {Footer}
     </div>
-  );
+);
+function App() {
+  return appRoot;
 }
 
 export default App;
