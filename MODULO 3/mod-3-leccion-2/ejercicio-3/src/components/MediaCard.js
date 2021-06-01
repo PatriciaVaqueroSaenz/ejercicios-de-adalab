6ef3f106670464741105cbbf6f0179fd2882b6a3
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-01 08:06:00 +0200

    add les 2 ex3

---
+++ MODULO 3/mod-3-leccion-2/ejercicio-3/src/components/MediaCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import foto from "../image/foto.jpg";
+import "../components/App.js";
+import "../stylesheet/App.css";
+
+// get our fontawesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+const MediaCard = (props) =>{
+      return (
+        <div className="App">
+            <header className="App-header">
+              <img src={foto} className="App-foto" alt="foto" />
+              <div className="App-header-title">
+                <h1 className="App-header-maintitle">{props.name}</h1>
+                <h3 className="App-header-subtitle">{props.date}</h3>
+              </div>
+            </header>
+            <main className="App-main">
+              <p className="App-main-paragraph">
+              {props.content}
+              </p>
+            </main>
+            <footer className="App-footer">
+              <p className="App-footer-paragraph">Leer más...</p>
+                  <div className="App-footer-container">
+                    <p className="App-footer-paragraph">37</p>
+                    <FontAwesomeIcon icon={faHeart} className="App-icon"/>
+                  </div>
+            </footer>
+        </div>
+      );
+}
+  
+export default MediaCard;
