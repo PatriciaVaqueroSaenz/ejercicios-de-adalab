fb68e4bde26062378e63caaef8574f768cb66c65
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-01 11:15:11 +0200

    add template react project

---
+++ MODULO 3/plantilla/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MediaCard from "./MediaCard";
+
+class App extends React.Component {
+  render() {
+    return (
+      <MediaCard />
+    );
+  }
+}
+
+export default App;
