0957588615439b2435f99254cc64b3dc38d5ca5f
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-01 07:44:17 +0200

    add les 1 ex1

---
+++ MODULO 3/mod-3-leccion-2/ejercicio-1/src/components/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import MediaCard from "./MediaCard";
+
+class App extends React.Component {
+  render() {
+    return (
+      <MediaCard />
+    );
+  }
+}
+
+export default App;
---
+++ MODULO 3/mod-3-leccion-2/ejercicio-1/src/components/MediaCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import foto from "../image/foto.jpg";
+import "../components/App.js";
+import "../stylesheet/App.css";
+
+// get our fontawesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+const Header = (
+    <header className="App-header">
+      <img src={foto} className="App-foto" alt="foto" />
+      <div className="App-header-title">
+        <h1 className="App-header-maintitle">Patricia Vaquero</h1>
+        <h3 className="App-header-subtitle">Lunes 26 de junio de 2017</h3>
+      </div>
+    </header>
+  );
+  const Main = (
+    <main className="App-main">
+      <p className="App-main-paragraph">
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their
+        default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      </p>
+    </main>
+  );
+  
+  const Footer = (
+    <footer className="App-footer">
+       <p className="App-footer-paragraph">Leer más...</p>
+          <div className="App-footer-container">
+            <p className="App-footer-paragraph">37</p>
+            <FontAwesomeIcon icon={faHeart} className="App-icon"/>
+          </div>
+    </footer>
+  );
+
+class MediaCard extends React.Component {
+    render() {
+      return (
+        <div className="App">
+            {Header}
+            {Main}
+            {Footer}
+        </div>
+      );
+    }
+  }
+  
+export default MediaCard;
