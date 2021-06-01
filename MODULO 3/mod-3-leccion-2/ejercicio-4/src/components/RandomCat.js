f1517f7b2dbd8b8e6093eb11accb4868316ac036
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-01 09:48:07 +0200

    add comments ex 4 les 2

--- MODULO 3/mod-3-leccion-2/ejercicio-4/src/components/RandomCat.js
+++ MODULO 3/mod-3-leccion-2/ejercicio-4/src/components/RandomCat.js
@@ -7,13 +7,18 @@ class RandomCat extends React.Component {
   render() {
     const randomCat = getRandomInteger(NUMBER_OF_CATS);
 
+    //se puede hacer asi 
+        //width={this.props.width !== null ? this.props.width:400} 
+        //height={this.props.height !== null ? this.props.height:200}
+        //src={`http://lorempixel.com/${width}/${height}/cats/${randomCat}`}
+
     return (
       <a href="http://lorempixel.com">
         <img
           src={`http://lorempixel.com/400/200/cats/${randomCat}`}
           alt="Random cat"
-          width={this.props.width} 
-          height={this.props.height}
+          width={this.props.width !== null ? this.props.width:400} 
+          height={this.props.height !== null ? this.props.height:200}
         />
       </a>
     );
