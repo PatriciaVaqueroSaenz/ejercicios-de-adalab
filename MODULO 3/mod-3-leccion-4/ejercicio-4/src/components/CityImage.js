92e71b3c7f0d1b538f1894f3e434dbc0382ce034
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-03 10:03:23 +0200

    add les 4 ex 4 chngs class comp

--- MODULO 3/mod-3-leccion-4/ejercicio-4/src/components/CityImage.js
+++ MODULO 3/mod-3-leccion-4/ejercicio-4/src/components/CityImage.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import '../stylesheet/App.css';
 
-const Images = {
-  Boston: 'https://st2.depositphotos.com/2001403/5248/i/600/depositphotos_52487179-stock-photo-boston-harbor-and-financial-district.jpg',
-  Praga: 'https://meraviglia.es/wp-content/uploads/2019/02/Cosas-a-tener-en-cuenta-en-Praga.jpg',
-  Tokyo: 'https://images.photowall.com/products/71333/mt-fuji-and-tokyo-skyline.jpg',
-  Sydney: 'https://rccl-h.assetsadobe.com/is/image/content/dam/royal/data/ports/sydney-australia/overview/sydney-australia-opera-house.jpg?$750x320$',
-  BuenosAires: 'https://pulsosocial.com/wp-content/uploads/2014/04/ba.jpg'
-}
-
-function CityImage(props) {
-  return (
+class CityImage extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.cities = {
+      
+        Boston: 'https://st2.depositphotos.com/2001403/5248/i/600/depositphotos_52487179-stock-photo-boston-harbor-and-financial-district.jpg',
+        Praga: 'https://meraviglia.es/wp-content/uploads/2019/02/Cosas-a-tener-en-cuenta-en-Praga.jpg',
+        Tokyo: 'https://images.photowall.com/products/71333/mt-fuji-and-tokyo-skyline.jpg',
+        Sydney: 'https://rccl-h.assetsadobe.com/is/image/content/dam/royal/data/ports/sydney-australia/overview/sydney-australia-opera-house.jpg?$750x320$',
+        BuenosAires: 'https://pulsosocial.com/wp-content/uploads/2014/04/ba.jpg'
+      
+    };
+  }
+  
+  render(){
+    const city = this.props.city;
+    const imageCity = this.cities[city];
+    
+    return (
       <>
-      <p>Tu destino es viajar a {props.city}</p>
-      <img className="image" src={Images[props.city]} alt={props.city} />
+      <p>Tu destino es viajar a {city}</p>
+      <img className="image" src={imageCity} alt={city} />
       </>
-  );
+    );
+  }
 
 
 }
