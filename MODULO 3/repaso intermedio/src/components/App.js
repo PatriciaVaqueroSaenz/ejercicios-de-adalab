0137082e4903d9287ab2d95d6dfa6e96ba76cd5a
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-05 11:21:58 +0200

    add folder repaso-intermedio

---
+++ MODULO 3/repaso intermedio/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ClubList from "./ClubList";
+import '../stylesheet/App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <ClubList />
+    );
+  }
+}
+
+export default App;
---
+++ MODULO 3/repaso intermedio/src/components/ClubList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Club from "./Club";
+import '../stylesheet/App.scss';
+
+class ClubList extends React.Component {
+  render() {
+    return (
+      <Club />
+    );
+  }
+}
+
+export default ClubList;
