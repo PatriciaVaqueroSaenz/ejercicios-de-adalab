c470532e4508e327bd582879a370bde73685a2fc
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-10 20:14:52 +0200

    add copy ex1 less 8 to ex2 less9

--- MODULO 3/mod-3-leccion-9/ejercicio-2/src/components/App.js
+++ MODULO 3/mod-3-leccion-9/ejercicio-2/src/components/App.js
@@ -1,38 +1,82 @@
-import "../stylesheet/App.scss";
-import React from "react";
-import { Link, Route, Switch } from 'react-router-dom';
-import Pricing from "./Pricing";
-import About from "./About";
-import Home from "./Home";
+import React from 'react';
+import getDataFromApi from '../services/api';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
+    this.state = {
+      users: [],
+      filterCity: '',
+      filterGender: '',
+      isLoading: true
+    };
+
+    getDataFromApi().then(data => {
+      console.log(data);
+      this.setState({
+        users: data,
+        isLoading: false
+      });
+    });
+
+    this.handleFilterCity = this.handleFilterCity.bind(this);
+    this.handleFilterGender = this.handleFilterGender.bind(this);
+  }
+
+  handleFilterCity (ev) {
+    this.setState({
+      filterCity: ev.target.value
+    });
+  }
+
+  handleFilterGender(ev) {
+    this.setState({
+      filterGender: ev.target.value
+    });
+  }
+
+  renderSeriesList() {
+    
+    return this.state.users
+      .filter(user => {
+        return user.city.toLowerCase().includes(this.state.filterCity.toLowerCase());
+      })
+      .filter(user => {
+        return user.gender.toLowerCase().includes(this.state.filterGender.toLowerCase());
+      })
+      .map(user => {
+        return <li key={user.name}>
+          <section>
+              <h3>Nombre: {user.name}</h3>
+              <img src={user.image} alt={user.name} />
+              <ul>
+                <li>Género: {user.gender}</li>
+                <li>Email: {user.email}</li>
+                <li>Nick: {user.username}</li>
+                <li>Ciudad: {user.city}</li>
+                <li>País: {user.country}</li>
+                <li>Edad: {user.age}</li>
+              </ul>
+            </section>
+          
+          </li>;
+      });
+  }
+
+  render() {
     return (
       <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/pricing">Pricing</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/pricing" component={Pricing} />
-        </Switch>
-      </main>
-    </div>
+        <h1>React Linkedin</h1>
+        <p>{this.state.isLoading ? 'Cargando..' : ''}</p>
+
+        <input type="text" onChange={this.handleFilterCity} placeholder="filter by city" />
+        <input type="text" onChange={this.handleFilterGender} placeholder="filter by gender" />
+
+        <ul>{this.renderSeriesList()}</ul>
+      </div>
     );
+  }
 }
 
 export default App;
--- MODULO 3/mod-3-leccion-9/ejercicio-2/src/index.js
+++ MODULO 3/mod-3-leccion-9/ejercicio-2/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheet/index.scss';
 import App from './components/App';
-import { HashRouter } from 'react-router-dom';
-
 
 ReactDOM.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
