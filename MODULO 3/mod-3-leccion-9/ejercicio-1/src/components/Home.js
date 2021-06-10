c74ca669bd41a78202887cc090b71cd3f2f3f13e
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-10 17:40:15 +0200

    add lesson 9 ex 1

---
+++ MODULO 3/mod-3-leccion-9/ejercicio-1/src/components/Home.js
@@ -0,0 +1,16 @@
+import "../stylesheet/App.scss";
+import React from "react";
+
+
+function Home() {
+  
+    return (
+      <>
+      <h1>Home</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis laborum esse minima aut deleniti sit atque a sequi at rerum repudiandae omnis delectus saepe doloremque dolorem, recusandae dignissimos aspernatur cupiditate.</p>
+      </>
+    );
+
+}
+
+export default Home;
