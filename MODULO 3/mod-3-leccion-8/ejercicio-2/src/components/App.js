206be1196667dbbc737b2ca97b0fc0c5cb0662cc
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-11 12:48:45 +0200

    add less 8 ex 2 change class repaso

--- MODULO 3/mod-3-leccion-8/ejercicio-2/src/components/App.js
+++ MODULO 3/mod-3-leccion-8/ejercicio-2/src/components/App.js
@@ -6,15 +6,23 @@ function App() {
   
   const [numbers, setNumbers] = useState([1, 4, 6, 8, 45, 89]);
 
+//----------------------------------------------------------
+
+  //FILTRO A LOS MAYORES QUE EL NUMERO INTRODUCIDO 
   const handleChange=(ev)=> {
-    
+    //SI HE INTRODUCIDO UN VALOR-->FILTRO NUMBERS
     if (ev.target.value) {
       return setNumbers(numbers.filter(number => number > parseInt(ev.target.value)));
+    //SI NO DEJO NUMBERS COMO ESTÁ
     } else {
       return setNumbers(numbers);
     }
   } 
 
+//----------------------------------------------------------
+
+
+  //FILTRO A LOS PARES SI ESTA CHECK
   const handleCheckBox=(ev)=> {
     
     if (ev.target.checked) {
@@ -24,23 +32,33 @@ function App() {
     }
   }
 
+//----------------------------------------------------------
+
+
+  //PINTA LA LISTA DE NUMEROS RESULTANTES DESPUES DE FILTRAR
+  const numberList = numbers.map((number, index) => 
+  <li key={index}>{number}</li>);
+
+//----------------------------------------------------------
+
+
   return (
+    
     <form>
-      
+      {/* FILTRO 1 */}
       <label>Muestra los mayores que...</label> 
       <input 
       type="text" 
       onChange={handleChange}/><br/>
-
+      {/* FILTRO 2 */}
       <label>Muestra los pares...</label> 
       <input 
       type="checkbox" 
       onChange={handleCheckBox}/>
       
+      {/* LISTA DE RESULTADOS DESPUÉS DE APLICAR FILTROS */}
       <ul>
-        {numbers.map((number, index) => 
-          <li key={index}>{number}</li>)
-        }
+        {numberList}
       </ul>
     
     </form>
