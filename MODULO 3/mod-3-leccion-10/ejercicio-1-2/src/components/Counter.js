ec838f50dfa2c4672139064e94b296da52d34f9f
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-14 07:33:13 +0200

    add less 9 ex 1 and 2

---
+++ MODULO 3/mod-3-leccion-10/ejercicio-1-2/src/components/Counter.js
@@ -0,0 +1,36 @@
+import "../stylesheet/App.scss";
+import React from "react";
+
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+  componentDidMount() {
+    console.log('Se está ejecuntando el método componentDidMount');
+    // guardamos el identificador del interval para limpiarlo en componentWillUnmount
+    this.intervalId = setInterval(this.incrementCounter, 1000);
+    // NOTA: guardamos el identificador en un atributo de la clase y
+    // no en el estado ya que no queremos pintar el identificador en el DOM
+  }
+  componentWillUnmount() {
+    console.log('Se está ejecuntando el método componentWillUnmount');
+    // limpiamos el interval
+    clearInterval(this.intervalId);
+  }
+  incrementCounter() {
+    console.log('Se está ejecuntando el método incrementCounter');
+    this.setState(prevState => {
+      return { counter: prevState.counter + 1 };
+    });
+  }
+  render() {
+    return <div>Contador: {this.state.counter}</div>;
+  }
+}
+
+export default Counter;
