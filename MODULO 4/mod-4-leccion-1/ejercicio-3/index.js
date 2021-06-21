33212eb8c1f426e65a153df89f0cca047a4e79ec
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-21 10:08:18 +0200

    add lesson 1 ex 3

---
+++ MODULO 4/mod-4-leccion-1/ejercicio-3/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const readFile = (fileName, callback) => {
+  fs.readFile(fileName, "utf8", (err, data) => {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      callback(data);
+    }
+  });
+};
+
+const writeFile = (fileName, fileContent, callback) => {
+  fs.writeFile(fileName, fileContent, (err) => {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      callback();
+    }
+  });
+};
+
+readFile("./inputFile.json", (fileContent) => {
+  const data = JSON.parse(fileContent);
+  data.user = "user";
+  writeFile("./output.txt", JSON.stringify(data), () => {
+    console.log("The file has been copied!");
+  });
+});
