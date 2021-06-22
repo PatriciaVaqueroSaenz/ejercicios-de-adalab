7e970356418d1a5dab1e20f1240573ed8f26d958
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-22 22:29:27 +0200

    add ex 2 lesson 3

---
+++ MODULO 4/mod-4-leccion-3/ejercicio-2/src/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+
+// create server
+const server = express();
+
+// set express middleware
+server.use(express.json());
+server.use(cors());
+
+// init express aplication
+const serverPort = 3000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// static server
+const staticServerPath = './public';
+server.use(express.static(staticServerPath));
+
+// users
+
+const users = []; // fake users data base
+
+// api endpoints
+
+server.post('/user', (req, res) => {
+  console.log('Body params:', req.body);
+  console.log('Body param userName:', req.body.userName);
+  console.log('Body param userEmail:', req.body.userEmail);
+
+  // add new user to daba base
+  users.push({
+    name: req.body.userName,
+    email: req.body.userEmail
+  });
+
+  res.json({
+    result: 'User created'
+  });
+});
+
+server.post('/users', (req, res) => {
+  console.log('Body params:', req.body);
+  console.log('Body param filterName:', req.body.filterByName);
+  console.log('Body param filterName:', req.body.filterByEmail);
+
+  const filterName = req.body.filterByName;
+  const filterEmail = req.body.filterByEmail;
+
+  const filteredUsers = users
+  .filter((user) => {
+    return filterName === "" ? true : user.name.toLowerCase().includes(filterName.toLowerCase());
+  })
+  .filter((user) => {
+    return filterEmail === "" ? true : user.email.toLowerCase().includes(filterEmail.toLowerCase());
+  });
+  
+  res.json({
+    result: filteredUsers
+  });
+});
