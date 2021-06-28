828231704732ac67bfa7e4ab20b4824f54e4f234
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-28 08:28:07 +0200

    add lesson 6 ex 3

---
+++ MODULO-4/mod-4-leccion-6/ejercicio-3/src/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const cors = require('cors');
+const Database = require('better-sqlite3');
+
+// create server
+const app = express();
+
+// set express middleware
+app.use(express.json());
+app.use(cors());
+
+// init express aplication
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// init and config data base
+const db = new Database('./src/database.db', {
+  // this line log in console all data base queries
+  verbose: console.log
+});
+
+// api endpoints
+
+app.get('/all-books-sorted-by-price', (req, res) => {
+  const query = db.prepare(`SELECT * FROM books ORDER BY price ASC`);
+  const books = query.all();
+  res.json(books);
+});
+
+app.get('/all-books-price-sup-5', (req, res) => {
+  const query = db.prepare(`SELECT * FROM books WHERE price > 5`);
+  const books = query.all();
+  res.json(books);
+});
+
+
+app.get('/books-in-stock', (req, res) => {
+  const query = db.prepare(`SELECT * FROM books WHERE stock > 0`);
+  const books = query.all();
+  res.json(books);
+});
+
+
+app.get('/book-id-1', (req, res) => {
+  const query = db.prepare(`SELECT * FROM books WHERE id = 1`);
+  const books = query.get();
+  res.json(books);
+});
+
+app.patch('/books-with-body-params/:id', (req, res) => {
+  const query = db.prepare(`UPDATE books SET name = ? WHERE id = ?`);
+  const books = query.run(req.body.name, req.params.id);
+  res.json(books);
+});
+
+app.patch('/books-with-body-params/change-stock', (req, res) => {
+  const query = db.prepare(`UPDATE books SET stock = 5`);
+  const books = query.run();
+  res.json(books);
+});
+
+app.post('/insert-with-body-params', (req, res) => {
+  const query = db.prepare(`INSERT INTO books (name, author, brief, price, stock, ebook) VALUES (?, ?, ?, ?, ?, ?)`);
+  const result = query.run(req.body.name, req.body.author, req.body.brief, req.body.price, req.body.stock, req.body.ebook);
+  res.json({
+    result: 'User created',
+    userId: result.lastInsertRowid
+  });
+});
+
+
+app.delete('/delete-with-url-params/:id', (req, res) => {
+  const query = db.prepare(`DELETE FROM users WHERE id = ?`);
+  const result = query.run(req.params.id);
+  console.log(result);
+    res.json({
+      result: 'User deleted'
+    });
+});
+
+app.delete('/delete-books-nostock-noebooks', (req, res) => {
+  const query = db.prepare(`DELETE FROM users WHERE stock = ? || ebook = ?`);
+  const result = query.run(0, 0);
+  console.log(result);
+    res.json({
+      result: 'User deleted'
+    });
+});
